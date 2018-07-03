c2f41184e6bfc57f2fcfc4651a0db33439766928
gxs <gxs>
2018-07-04 01:45:38 +0800

    #update 更新工具

--- tool/weight.py
+++ tool/weight.py
@@ -1,4 +1,6 @@
 #! --*-- coding: utf-8 --*--
+from objc._objc import NULL
+
 __author__ = 'gaoxingsheng'
 
 # 体重
@@ -30,41 +32,53 @@ class Application(Frame):
             if fat < info[0]:
                 return info[1]
 
-        return "体脂率数据不正常"
+        return "数据不正常"
 
-    def boday_fat(self):
+    def boday_fat(self, v1 = NULL, v2 = NULL):
         print "hi there, everyone!"
-        height = float(self.height_entry.get()) / 100
-        weight = float(self.weight_entry.get())
-        fat = weight / (height * height)
-        self.result.set(self.fat_bmi(fat) + "%(fat).2f"%{'fat':fat})
+        try:
+            height = float(self.height_entry.get()) / 100
+            weight = float(self.weight_entry.get())
+            height = max(height, 1)
+            fat = weight / (height * height)
+            self.result.set(self.fat_bmi(fat) + "%(fat).2f"%{'fat':fat})
+        except Exception as e:
+            self.result.set("无效数据")
 
     def createWidgets(self):
-        label = Label(self, text = "身高")
+        font_normal = ('Helvetica', '22', 'bold')
+        font_result = ('Helvetica', '40', 'bold')
+        label = Label(self, text = "身高(cm)", font = font_normal)
         label.pack(fill=X, expand=1)
 
-        self.height_entry = Entry(self, text = "输入身高")
+        var = StringVar()
+        self.height_entry = Entry(self, text = "0", font = font_normal)
+        self.height_entry['xscrollcommand'] = self.boday_fat
         self.height_entry.pack(fill=X)
 
-        label = Label(self, text = "体重")
+        label = Label(self, text = "体重(kg)", font = font_normal)
         label.pack()
 
-        self.weight_entry = Entry(self, text = "体重")
+        self.weight_entry = Entry(self, text = "体重", font = font_normal)
+        self.weight_entry['xscrollcommand'] = self.boday_fat
         self.weight_entry.pack(fill=X)
 
-        self.QUIT = Button(self, text = "体脂率")
-        self.QUIT["fg"] = "red"
-        self.QUIT["command"] = self.boday_fat
-        self.QUIT.pack(fill=X)
+        btn_quit = Button(self, text = "体脂率", font = font_normal)
+        btn_quit["fg"] = "red"
+        btn_quit["command"] = self.boday_fat
+        btn_quit.pack()
+        self.btn_quit = btn_quit
 
-        self.label_result = Label(self, textvariable = self.result)
+
+        label = Label(self, text = "    ", font = font_normal)
+        label.pack(fill=X)
+
+        self.label_result = Label(self, textvariable = self.result, font = font_result)
         self.label_result.pack(fill=X)
-
-
 
 
 root = Tk()
 root.title = "体重"
-root.geometry("300x300+300+0")
+root.geometry("500x300+500+0")
 app = Application(master=root)
 app.mainloop()
